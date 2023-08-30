3bddc9b6bfe3f68b3679f64d615b8c11e121773c
Wilson Chen <wilsonchen901@gmail.com>
2023-08-30 16:15:45 -0400

    finish credential styling and layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./style.scss";
 import Header from "./components/Header";
 import About from "./components/About";
 import Programs from "./components/Programs";
+import Credentials from "./components/Credentials";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <About />
       <Programs />
+      <Credentials />
     </div>
   );
 }
---
+++ src/components/Credentials.jsx
@@ -0,0 +1,34 @@
+
+import credentials_img from "../assets/BenefitsPageGraphic.png";
+import Button from "./Button";
+
+const Credentials = () => {
+  return (
+    <div className="credentials">
+        <img src={credentials_img} alt="" />
+        <div className="credential-details">
+            <div className="credential-headline">
+                <span>START YOUR FITNESS JOURNEY WITH </span>
+                <span id="gold">MOMENTUM</span>
+            </div>
+            <div className="credential-text">
+                <span id="credential-title">CREDENTIALS</span>
+                <span>Owner / Personal Trainer</span>
+                <span>NASM Certified Personal Trainer</span>
+                <span>NASM Corrective Exercise Specialist</span>
+                <span>NASM Performance Enhancing Specialist</span>
+                <span>ISSA Strength & Conditioning Coach</span>
+                <span>ISSA Nutrition Coach</span>
+                <span>ISSA Certified Personal Trainer</span>
+                <span>NFPT Functional Training Specialist</span>
+                <span>CPR/AED Certified</span>
+                <span id="credential-desc">Kevin loves his job as a personal trainer and coach. He believes that personal trainers and coaches are the frontline of helping individuals take their first steps in changing their lives. Kevin is extremely experienced with transformations, helping individuals stay consistent and accountable for their fitness goals. He is willing to take on any client who loves to be challenged and wants to be the best version of themselves.</span>
+            </div>
+            <Button className="credential-button" text={"Join Now"}/>
+        </div>
+
+    </div>
+  );
+};
+
+export default Credentials;
