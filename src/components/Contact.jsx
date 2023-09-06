2f1b9f8b4d657082bf104e99871bfecb9bdf7b40
Wilson Chen <wilsonchen901@gmail.com>
2023-09-06 15:55:44 -0400

    finsih form submit functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 
 const Contact = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [number, setNumber] = useState('');
+    const [type, setType] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setName('');
+        setEmail('');
+        setNumber('');
+        setType('');
+    }
 
     return (
         <div className="contact-container">
@@ -16,11 +30,23 @@ const Contact = () => {
                 target="_blank"
                 method="POST"
                 action="https://formsubmit.co/projectmovementstudio@gmail.com"
+                onSubmit={handleSubmit}
                 >
-                    <input type="text" placeholder='NAME' />
-                    <input type="text" placeholder='EMAIL' />
-                    <input type="text" placeholder='PHONE NUMBER' />
-                    <select>
+                    <input type="text" 
+                    placeholder='NAME' 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)}/>
+                    <input type="text" 
+                    placeholder='EMAIL' 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="text" 
+                    placeholder='PHONE NUMBER' 
+                    value={number}
+                    onChange={(e) => setNumber(e.target.value)}/>
+                    <select 
+                    onChange={(e) => setType(e.target.value)}
+                    value={type}>
                         <option value="default"  selected disabled>Which Program are you interested in?</option>
                         <option value="personal">Personal Training</option>
                         <option value="group">Group Training</option>
