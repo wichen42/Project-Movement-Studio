5de98c7d25fc4592e26e651bb51714ced56992d1
Wilson Chen <wilsonchen901@gmail.com>
2023-09-14 16:08:54 -0400

    basic styling for news letter landing page

--- src/components/NewsLetterLandingPage.jsx
+++ src/components/NewsLetterLandingPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import temp_headshot from "../assets/LogoGold.png"
 
 const NewsLetterLandingPage = () => {
   return (
-    <div>NewsLetterLandingPage</div>
+    <div className='newsletter-container'>
+      <div className="newsletter-main">
+          <div className="newsletter-headshot">
+            <img src={temp_headshot} alt="headshot" />
+          </div>
+
+          <div className="newsletter-details">
+            <span id='newsletter-title'>Welcome to Project MVMT!</span>
+            <span id='newsletter-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat commodi et eligendi molestiae animi magni iusto perspiciatis repellat laboriosam necessitatibus adipisci voluptatibus reiciendis illo, libero rerum hic? Magnam, voluptates inventore?</span>
+            <div className='newsletter-form'>
+              <form action="#">
+                <input type="text" placeholder='Email Address' />
+                <button type="submit">Subscribe</button>
+              </form>
+              <span>We respect your privacy. Unsubscribe at any time.</span>
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
