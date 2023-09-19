de17ec36b932e30324bc9dda01b572329b5f04ec
Wilson Chen <wilsonchen901@gmail.com>
2023-09-19 17:11:24 -0400

    signup page styling

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import temp_login_bg from "../assets/login_background.jpg";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { IconContext } from 'react-icons'
+import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+    const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <div className='signup-container'>
+            <div className="signup-form">
+                <span>Sign Up</span>
+                <form action="#">
+                    <div className="signup-name">
+                        <input type="text" name="first_name" placeholder="First Name" />
+                        <input type="text" name="last_name" placeholder="Last Name" />
+                    </div>
+                    <input type="text" name="email" placeholder='Email Address' />
+                    <div className="password-container">
+                        <input type={showPassword ? `text` :  `password`} name="password" placeholder='Password' />
+                        <IconContext.Provider value={{color: "rgb(51,51,51)"}}>
+                            <div className="password-toggle" onClick={() => setShowPassword(!showPassword)}>
+                                {showPassword ? 
+                                    <AiOutlineEye /> : 
+                                    <AiOutlineEyeInvisible />
+                                }
+                            </div>
+                        </IconContext.Provider>
+                    </div>
+                    <input type="text" name="phone_number" placeholder="Phone Number" />
+                    <button>Sign Up</button>
+                </form>
+                <div className="signup-text">
+                    <span>Already Have An Account? </span>
+                    <span className="signup-link" onClick={() => navigate('/login')}>Login</span>
+                </div>
+            </div>
+            <div className="signup-image">
+                <img src={temp_login_bg} alt="" />
+            </div>
+        </div>
+
+    )
 }
 
 export default Signup
