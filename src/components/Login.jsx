345ff07c74a466c8d05c18f47b12db3d30e2cd50
Wilson Chen <wilsonchen901@gmail.com>
2023-09-19 18:04:12 -0400

    add values to login inputs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,15 +7,29 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
     const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     return (
         <div className='login-container'>
             <div className="login-form">
                 <span>Login</span>
                 <form action="#">
-                    <input type="text" name="email" placeholder='Email Address' />
+                    <input 
+                    type="text" 
+                    name="email" 
+                    placeholder='Email Address' 
+                    value={email}
+                    onChange={(e) => {setEmail(e.target.value)}}
+                    required />
                     <div className="password-container">
-                        <input type={showPassword ? `text` :  `password`} name="password" placeholder='Password' />
+                        <input 
+                        type={showPassword ? `text` :  `password`} 
+                        name="password" 
+                        placeholder='Password' 
+                        value={password}
+                        onChange={(e) => {setPassword(e.target.value)}}
+                        required />
                         <IconContext.Provider value={{color: "rgb(51,51,51)"}}>
                             <div className="password-toggle" onClick={() => setShowPassword(!showPassword)}>
                                 {showPassword ? 
