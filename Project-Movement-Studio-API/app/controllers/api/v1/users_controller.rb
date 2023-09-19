a9f7111652fb82149c33984b64606f4c58a1f2c3
Eugene Kofman <eugenekofman@gmail.com>
2023-09-19 03:26:00 -0400

    complete session controller, most of user controller

---
+++ Project-Movement-Studio-API/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,70 @@
+class Api::V1::UsersController < ApplicationController
+    wrap_parameters include: User.attribute_names + [:username, :password, :firstName. :lastName, :phoneNumber]
+
+    def index
+        unless !current_user_admin? 
+            @users = User.all
+            render :index
+        else
+            render json: { errors: ["You do not have permission to view all users"] }, status: 401
+        end
+    end
+
+    def show
+        id = params[:id]
+        unless id == current_user || current_user_admin?
+            render json: { errors: ["You do not have permission to view this user"] }, status: 401
+            return
+        end
+        render :show
+    end
+
+    def update
+        id = params[:id]
+        unless id == current_user.id || current_user_admin?
+            render json: { errors: ["You do not have permission to update this user"] }, status: 401
+            return
+        end
+        @user = User.find_by(id: id)
+        if @user.update(user_params)
+            render :show
+        else
+            render json: { errors: @user.errors.full_messages }, status: 422
+        end
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            render :show
+        else
+            render json: { errors: @user.errors.full_messages }, status: 422
+        end
+    end
+
+    ## Needs more thought. Do we want to allow users to delete their accounts? If so, how do we handle the data? 
+    # def destroy
+    #     id = params[:id]
+    #     @user = User.find_by(id: id)
+    #     if id == current_user.id
+    #         render json: { body: ["Please email the adming to have your account removed"] }, status: 200
+    #     elsif current_user_admin?
+    #         render json: { body: ["You do not have permission to delete this user"] }, status: 401
+    #         return
+    #     end
+    #     if @user.destroy
+    #         render json: {}
+    #     else
+    #         render json: { errors: @user.errors.full_messages }, status: 422
+    #     end
+    # end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :first_name, :last_name, :password, :phone_number, :username)
+    end
+
+    def requirementsd
+
+end
--- Project-Movement-Studio-API/app/controllers/application_controller.rb
+++ Project-Movement-Studio-API/app/controllers/application_controller.rb
@@ -3,10 +3,67 @@ class ApplicationController < ActionController::API
 
     include ActionController::RequsertForgeryProtection
 
+    rescue_from StandardError, with: :unhandled_error
+
+    rescue_from ActionController::InvalidAuthenticityToken,
+        with: :invalid_authenticity_token
+
     protect_from_forgery with: :exception
 
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def current_user_admin?
+        current_user.admin
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token!
+        @current_user = nil
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { errors: ["You must be logged in to perform this action"] }, status: 401
+        end
+    end
+
+    def require_admin
+        unless current_user_admin?
+            render json: { errors: ["You must be an admin to perform this action"] }, status: 401
+        end
+    end
+
+    
 
     private
+
+    def unhandled_error(error)
+        if request.accepts.first.html?
+            raise error
+        else
+            @message = "#{error.class} - #{error.message}"
+            @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+            render 'api/errors/internal_server_error',
+                status: :internal_server_error
+                        
+            logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+        end
+    end
+
+    def invalid_authenticity_token
+        render json: { message: 'Invalid authenticity token, have you checked if you might have been forged? ' }, 
+            status: :unprocessable_entity
+    end
 
     def attach_authenticity_token
         headers['X-CSRF-Token'] = form_authenticity_token(session)
