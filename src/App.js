af8bafa346ccce763f29a1530ba30818eba0a16e
Wilson Chen <wilsonchen901@gmail.com>
2023-09-19 16:58:21 -0400

    add signup route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import  { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "../src/components/Home";
 import NewsLetterLandingPage from "./components/NewsLetterLandingPage";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<Home/>}/>
           <Route path="newsletter" element={<NewsLetterLandingPage />} />
           <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>    
     </div>
