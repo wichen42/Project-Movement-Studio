8779db60e16d8f597eb9078c5767f26d0824db6a
Wilson Chen <wilsonchen901@gmail.com>
2023-09-19 16:39:35 -0400

    password toggle

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,36 @@
 import login_bg from "../assets/login_background.jpg";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { IconContext } from 'react-icons';
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div className='login-container'>
-        <div className="login-form">
-            <span>Login</span>
-            <form action="#">
-                <input type="text" name="email" placeholder='Email Address' />
-                <input type="text" name="password" placeholder='Password' />
-                <button>Login</button>
-            </form>
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <div className='login-container'>
+            <div className="login-form">
+                <span>Login</span>
+                <form action="#">
+                    <input type="text" name="email" placeholder='Email Address' />
+                    <div>
+                        <input type={showPassword ? `text` :  `password`} name="password" placeholder='Password' />
+                        <IconContext.Provider value={{color: "rgb(51,51,51)"}}>
+                            <div className="password-toggle" onClick={() => setShowPassword(!showPassword)}>
+                                {showPassword ? 
+                                    <AiOutlineEye /> : 
+                                    <AiOutlineEyeInvisible />
+                                }
+                            </div>
+                        </IconContext.Provider>
+                    </div>
+                    <button>Login</button>
+                </form>
+            </div>
+            <div className="login-image">
+                <img src={login_bg} alt="" />
+            </div>
         </div>
-        <div className="login-image">
-            <img src={login_bg} alt="" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Login
